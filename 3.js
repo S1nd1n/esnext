0884fd125937eb5cb75d5d9778a90fde9e1fd26e
S1nd1n <krez2010@gmail.com>
2022-05-18 08:52:30 +0300

    Day 2

--- 3.js
+++ 3.js
@@ -14,14 +14,60 @@ for (let prop in c1) {
     console.log(prop);
 }
 
-let fn =function(){}
+let fn = function () { }
 fn();
 
 let Course = class {
     title;
-    getTitle(){return this.title}
+    getTitle() { return this.title }
 }
 
 let c2 = new Course();
 c2.title = 'MongoDB';
 console.log(c2);
+
+class Math2 {
+    static issOdd(n) {
+        return !!(n % 2);
+    }
+}
+
+console.log(Math2.issOdd(35));
+console.log(Math2.issOdd(32));
+
+class EducationalProduct {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+    props() {
+        for (let i in this) {
+            console.log(`${i} = ${this[i]}`)
+        }
+    }
+}
+
+class Webinar extends EducationalProduct {
+    constructor(title, price, qty) {
+        super(title, price);
+        this.quantity = qty;
+    }
+}
+class Practice extends EducationalProduct {
+    constructor(title, price, qty) {
+        super(title, price);
+        this.quantity = qty;
+    }
+}
+
+let w1 = new Webinar('Как собират Parsel.js', 1, 100);
+let w2 = new Webinar('Как тестироваться с Jest.js', 1, 200);
+let p1 = new Practice('MongoDB', 8, 20);
+w1.props();
+w2.props();
+p1.props();
+
+console.log(w1.constructor, w2.constructor, p1.constructor);
+console.log(w1 instanceof EducationalProduct);
+console.log(w1 instanceof Webinar);
+console.log(w1 instanceof Practice);
---
+++ 4.js
@@ -0,0 +1,7 @@
+import sub from "./sub.js";
+import { pi, i, mul, Course } from "./sub.js"
+console.log(sub(100, 10));
+console.log(pi);
+console.log(i);
+console.log(mul(2, 3));
+console.log(new Course());
